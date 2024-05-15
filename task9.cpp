d62af2eec4b8422cde7bf3d51e8052883bf0f34f
webusinka <veronikabykova05@gmail.com>
2024-05-16 00:05:24 +0700

    Added task 9 / 10

--- task9.cpp
+++ task9.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-#include <numeric>
 #include <string>
+
 using namespace std;
+
+vector<string> remove_and_sort_by_descending(vector<string> car_brands) {
+
+    car_brands.erase(remove_if(car_brands.begin(), car_brands.end(), [](const std::string& brand) {
+        return brand.find("BM") != string::npos;
+    }), car_brands.end());
+
+    sort(car_brands.rbegin(), car_brands.rend());
+    
+    return car_brands;
+}
 
 int main() {
     //задание 9.
     cout << "\tTask #9" << endl;
 
+    vector<string> car_brands = {"BMW", "Mercedes", "Audi", "Skoda", "Volvo", "Subaru", "Lamborghini", "BMWi"};
+    
+    vector<string> result = remove_and_sort_by_descending(car_brands);
+
+    for (const auto& brand : result) {
+        cout << brand << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
